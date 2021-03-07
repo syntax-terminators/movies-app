73a5e692f848c67633f5abcddde07f8606a0720d
eliasd1 <eliasdadoush23@gmail.com>
2021-03-07 21:06:57 +0200

    added localstorage

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,19 @@
-$('#genre').on('change', triggerSubmit)
-$('#year').on('change', triggerSubmit)
+$('#genre').on('change', triggerGenreSubmit)
+$('#year').on('change', triggerYearSubmit)
 
-function triggerSubmit(){
+function triggerGenreSubmit(){
     $('#filterForm').trigger('submit');
+    localStorage.setItem('genreVal', $(this).val())
 }
+function triggerYearSubmit(){
+    $('#filterForm').trigger('submit');
+    localStorage.setItem('yearVal', $(this).val())
+}
+$(document).ready(function() {
+    if (localStorage.genreVal) {
+        $('#genre').val( localStorage.genreVal);
+    }
+    if(localStorage.yearVal){
+        $('#year').val(localStorage.yearVal)
+    }
+});
