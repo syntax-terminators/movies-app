aa0fb9c792775869476ba0b3ba93a5e42abc15e6
motasemAlsqoor <motasem900khaled@gmail.com>
2021-03-07 17:03:49 +0200

    filter by genra

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /***************************************************
 *****************Dependencies***********************
 ****************************************************/
-const express =require('express');
-const cors=require('cors');
-const dotenv=require('dotenv');
-const pg=require("pg");
-const superAgent=require("superagent");
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const pg = require("pg");
+const superAgent = require("superagent");
 const ejs = require('ejs');
 
 /***************************************************
@@ -22,26 +22,26 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-const PORT=process.env.PORT;
+const PORT = process.env.PORT;
 
 /***************************************************
 *****************ROUTS******************************
 ****************************************************/
 
 // HOME PAGE
-app.get("/",homePageHandler);
+app.get("/", homePageHandler);
 
 // DETAILS PAGE
-app.get("/details/:id",detailsPageHandler);
+app.get("/details/:id", detailsPageHandler);
 
 // LIBRARY PAGE
-app.get("/library",libraryPageHandler);
+app.get("/library", libraryPageHandler);
 
 // QUIZ PAGE
-app.get("/quiz",quizPageHandler);
+app.get("/quiz", quizPageHandler);
 
 // SEARCH PAGE 
-app.get("/search",searchHandler);
+app.get("/search", searchHandler);
 
 
 
@@ -50,21 +50,21 @@ app.get("/search",searchHandler);
 /***************************************************
 *****************HANDLER*****************************
 ****************************************************/
-function homePageHandler(req,res) {
-    getHomePageData(req,res);
+function homePageHandler(req, res) {
+    getHomePageData(req, res);
 }
-function detailsPageHandler(req,res) {
-    getDetailsData(req,res);
+function detailsPageHandler(req, res) {
+    getDetailsData(req, res);
     //res.render("pages/details");
 }
-function libraryPageHandler(req,res) {
+function libraryPageHandler(req, res) {
     res.render("pages/library");
 }
-function quizPageHandler(req,res) {
+function quizPageHandler(req, res) {
     res.render("pages/quiz");
 }
-function searchHandler(req,res) {
-    getSearchData(req,res)
+function searchHandler(req, res) {
+    getSearchData(req, res)
 }
 
 
@@ -72,86 +72,86 @@ function searchHandler(req,res) {
 /***************************************************
 *****************GETTER*****************************
 ****************************************************/
-function getHomePageData(req,res) {
-    
-    let apiUrl='https://api.themoviedb.org/3/movie/popular?';
-    let query={
-        api_key:process.env.MOVIE_API_KEY,
-        language:"en-US",
-        page:1
-    }
-    superAgent
-    .get(apiUrl)
-    .query(query)
-    .then(data=>{
-        console.log("*******************************HOME********************************")
-        console.log(req.query);//the default val ={} no query provided
-        console.log(JSON
-            .parse(data.text)
-            .results);
-        var movies=JSON
-        .parse(data.text)
-        .results
-        .filter(x=>{
-            if(req.query.genraId){//genra id provided
-                return x.genre_ids.some(x=>x==req.query.genraId);
-            }else{
-            return true;
-            }   
-        })
-        .map(element=>new Movie(element));
+function getHomePageData(req, res) {
 
-        //console.log(movies)
-        
-        res.render("index",{movies:movies});
-    })
-    .catch(error=>{
-        res.render("error",{"error":error});
-    })
-}
-function getDetailsData(req,res) {
-    let movieId=req.params.id;
-    let apiUrl=`https//api.themoviedb.org/3/movie/${movieId}?`;
-    let query={
-        api_key:process.env.MOVIE_API_KEY,
-        language:"en-US",
-        page:1,
-        append_to_response:"credits"
+    let apiUrl = 'https://api.themoviedb.org/3/movie/popular?';
+    let query = {
+        api_key: process.env.MOVIE_API_KEY,
+        language: "en-US",
+        page: 1
     }
     superAgent
-    .get(apiUrl)
-    .query(query)
-    .then(data=>{
-        var movie=new Movie(JSON.parse(data.text));
-        console.log("movie",movie);
-        res.render("index",{movies:movie});      
-    })
-    .catch(error=>{
-        res.render("error",{"error":error});
-    })
+        .get(apiUrl)
+        .query(query)
+        .then(data => {
+            var movies = JSON
+                .parse(data.text)
+                .results
+                .filter(x => {
+                    if (req.query.genraId) {//genra id provided
+                        if (req.query.genraId !== "All") {//genre id not All
+                            return x.genre_ids.some(x => x == req.query.genraId);
+                        } return true;
+                    } return true;
+
+                })
+                .filter(x => {
+                    if (req.query.year) {//year  provided
+                        if (req.query.year !== "All") {//year id not All
+                            //solution here
+                        } return true;
+                    } return true;
+                })
+                .map(element => new Movie(element));
+            movies.forEach(element => {
+                console.log(element.date)
+            });
+            res.render("index", { movies: movies });
+        })
+        .catch(error => {
+            res.render("error", { "error": error });
+        })
 }
-function getSearchData(req,res) {
-    console.log(req.body);
-    let apiUrl =`https://api.themoviedb.org/3/search/movie?query='${req.query.query}'`
-    let query={
-        api_key:process.env.MOVIE_API_KEY
+function getDetailsData(req, res) {
+    let movieId = req.params.id;
+    let apiUrl = `https//api.themoviedb.org/3/movie/${movieId}?`;
+    let query = {
+        api_key: process.env.MOVIE_API_KEY,
+        language: "en-US",
+        page: 1,
+        append_to_response: "credits"
     }
     superAgent
-    .get(apiUrl)
-    .query(query)
-    .then(data=>{
-        var movies=JSON
-        .parse(data.text)
-        .results
-        .map(element=>new Movie(element));
-        console.log(movies)
-        res.render("index",{movies:movies});
-        
-    })
-    .catch(error=>{
-        console.log(error);
-        res.render("error",{"error":error});
-    })
+        .get(apiUrl)
+        .query(query)
+        .then(data => {
+            var movie = new Movie(JSON.parse(data.text));
+            res.render("index", { movies: movie });
+        })
+        .catch(error => {
+            res.render("error", { "error": error });
+        })
+}
+function getSearchData(req, res) {
+    let apiUrl = `https://api.themoviedb.org/3/search/movie?query='${req.query.query}'`
+    let query = {
+        api_key: process.env.MOVIE_API_KEY
+    }
+    superAgent
+        .get(apiUrl)
+        .query(query)
+        .then(data => {
+            var movies = JSON
+                .parse(data.text)
+                .results
+                .map(element => new Movie(element));
+            res.render("index", { movies: movies });
+
+        })
+        .catch(error => {
+            console.log(error);
+            res.render("error", { "error": error });
+        })
 
 }
 
@@ -162,7 +162,7 @@ function getSearchData(req,res) {
 /***************************************************
 *****************DATA MODEL*************************
 ****************************************************/
-function Movie(movie){
+function Movie(movie) {
     this.movieID = movie.id;
     this.description = movie.overview || 'Not Available';
     this.title = movie.title || 'Not Available';
